8de802d59d05566b593aee741beea48e33c06de6
Liam Stabeno <liamstabeno@gmail.com>
2017-02-24 16:41:15 -0800

    cleaned up line indentation, added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,36 @@
 // Back-End
 var serve = function(input){
   var userArray = convertToArray(input); //  Converts to Array
-  var amendedArray = amendArray(userArray); //Amends Array
+  var amendedArray = amendArray(userArray); // Amends Array
 
-  for(i=0; i<amendedArray.length; i++){
-    $(".results").append("<li>"+amendedArray[i]+"</li>");  // Populates the list on the webpage with info from the "amendedArray"
-  };
+    for(i=0; i<amendedArray.length; i++){
+      $(".results").append("<li>"+amendedArray[i]+"</li>");  // (Not Sure if this should be in front end)??? Populates the list on the webpage with info gathered from the "amendedArray"
+    };
 };
+
 var convertToArray = function(input){ //  Converts the users input into a "newArray" from 1 - the input.
-var newArray = [];
-for(var i = 1; i <= input; i++){
-  newArray.push(i);
-};
-return newArray;
+  var newArray = [];
+    for(var i = 1; i <= input; i++){
+      newArray.push(i);
+    };
+    return newArray;
 };
 
 var amendArray = function(newArray){ //   Creates and returns an "amendedArray" with ping, pong, and pingpong in the appropriate places.
 
-var amendedArray = newArray;
+  var amendedArray = newArray;
 
-for(var i = 1; i <=newArray.length; i++){
-  if (newArray[i]%5===0 && newArray[i]%3===0){
-    amendedArray.splice(i,1,"ping-pong");
-  };
-  if (newArray[i]%3===0){
-    amendedArray.splice(i,1,"ping")
-  };
-  if (newArray[i]%5===0){
-    amendedArray.splice(i,1,"pong");
-  };
-};
-console.log(amendedArray);
+    for(var i = 1; i <=newArray.length; i++){
+      if (newArray[i]%5===0 && newArray[i]%3===0){
+        amendedArray.splice(i,1,"ping-pong");
+      };
+      if (newArray[i]%3===0){
+        amendedArray.splice(i,1,"ping")
+      };
+      if (newArray[i]%5===0){
+        amendedArray.splice(i,1,"pong");
+      };
+    };
 return amendedArray;
 };
 
